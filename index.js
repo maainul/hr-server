94f0ecc9bdb26e573c7643749c71a5c67e94e629
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-07-31 14:11:07 +0600

    test slack

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-
-import cors from 'cors'
-import morgan from 'morgan'
+import cors from "cors";
+import morgan from "morgan";
 import colors from "colors";
 import dotenv from "dotenv";
-import express from 'express'
-import cookieParser from 'cookie-parser';
-import connectDB from './dbConnection.js';
-import setupRoutes from './appRoute.js';
-
+import express from "express";
+import cookieParser from "cookie-parser";
+import connectDB from "./dbConnection.js";
+import setupRoutes from "./appRoute.js";
 
 //configure env
-dotenv.config()
+dotenv.config();
 
 //databse config
-connectDB()
+connectDB();
 
 //rest object
-const app = express()
+const app = express();
 
 //middelwares
 
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan("dev"));
 
-app.use(cookieParser())
+app.use(express.json());
 
-app.use(cors({
+app.use(cookieParser());
+
+app.use(
+  cors({
     origin: ["http://localhost:3000", "https://hr.netlify.app"],
     credentials: true,
-}))
+  })
+);
 
-setupRoutes(app)
+setupRoutes(app);
 
 const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
-    console.log(
-        `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-            .white
-    );
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
-
 
 export default app;
