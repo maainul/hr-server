d65dddf0e081ae5ce56508c5d480d95273fad927
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-08-19 18:10:47 +0600

    update dpt service

--- services/departmentServices.js
+++ services/departmentServices.js
@@ -1,40 +1,50 @@
-import DepartmentModel from "../model/departmentModel.js"
-import { pagination } from './pagination.js';
-
+import DepartmentModel from "../model/departmentModel.js";
+import { pagination } from "./pagination.js";
 
 export const getAllDepartmentWithPaginationService = async ({ req }) => {
-    try {
-        // Query Param For Search
-        const { search = '', sort = 'latest', page, limit } = req.query;
+  try {
+    // Query Param For Search
+    const { search = "", sort = "latest", page, limit, status } = req.query;
 
-        //Conditions for searching filters
-        let queryObject = {}
+    //Conditions for searching filters
+    let queryObject = {};
 
-        //Check Search for Query
-        if (search) {
-            //Search by name or dptCode
-            queryObject = {
-                $or: [
-                    { name: { $regex: search, $options: "i" } },
-                    { dptCode: { $regex: search, $options: "i" } }
-                ]
-            }
-        }
-
-        // Build Mongoose Query Based on Search
-        let queryResult = DepartmentModel.find(queryObject)
-
-        //Sorting
-        if (sort === 'latest') queryResult = queryResult.sort('-createdAt')
-        if (sort === 'oldest') queryResult = queryResult.sort('createdAt')
-        if (sort === 'a-z') queryResult = queryResult.sort('name')
-        if (sort === 'z-a') queryResult = queryResult.sort('-name')
-
-        const paginationResult = await pagination(page, limit, queryResult, queryObject, DepartmentModel)
-
-        return paginationResult
-
-    } catch (error) {
-        throw new Error(`Error in getAllDepartmentWithPaginationService: ${error.message}`);
+    if (status) {
+      queryObject.status = status;
     }
-}
+
+    //Check Search for Query
+    if (search) {
+      //Search by name or dptCode
+      queryObject = {
+        $or: [
+          { name: { $regex: search, $options: "i" } },
+          { dptCode: { $regex: search, $options: "i" } },
+        ],
+      };
+    }
+
+    // Build Mongoose Query Based on Search
+    let queryResult = DepartmentModel.find(queryObject);
+
+    //Sorting
+    if (sort === "latest") queryResult = queryResult.sort("-createdAt");
+    if (sort === "oldest") queryResult = queryResult.sort("createdAt");
+    if (sort === "a-z") queryResult = queryResult.sort("name");
+    if (sort === "z-a") queryResult = queryResult.sort("-name");
+
+    const paginationResult = await pagination(
+      page,
+      limit,
+      queryResult,
+      queryObject,
+      DepartmentModel
+    );
+
+    return paginationResult;
+  } catch (error) {
+    throw new Error(
+      `Error in getAllDepartmentWithPaginationService: ${error.message}`
+    );
+  }
+};
--- services/pagination.js
+++ services/pagination.js
@@ -1,31 +1,34 @@
+export const pagination = async (
+  page,
+  limit,
+  queryResult,
+  queryObject,
+  model
+) => {
+  let numberOfPages = 0;
+  let startPageData = 0;
+  let upToPageTotalData = 0;
+  let totalDataCount = 0;
+  if (page !== undefined || limit !== undefined) {
+    const pageNumber = Number(page);
+    const limitNumber = Number(limit);
+    const skip = (pageNumber - 1) * limitNumber;
 
+    queryResult = queryResult.skip(skip).limit(limitNumber);
+    totalDataCount = await model.countDocuments(queryObject);
+    numberOfPages = Math.ceil(totalDataCount / limit);
+    upToPageTotalData = Math.min(pageNumber * limitNumber, totalDataCount);
+    startPageData = skip + 1;
+  }
+  const pageDataCount = await model.countDocuments(queryResult);
+  const data = await queryResult;
 
-export const pagination = async (page, limit, queryResult, queryObject, model) => {
-    let numberOfPages = 0;
-    let startPageData = 0;
-    let upToPageTotalData = 0;
-    let totalDataCount = 0;
-    if (page !== undefined || limit !== undefined) {
-        const pageNumber = Number(page);
-        const limitNumber = Number(limit);
-        const skip = (pageNumber - 1) * limitNumber;
-
-        queryResult = queryResult.skip(skip).limit(limitNumber)
-        totalDataCount = await model.countDocuments(queryObject)
-        numberOfPages = Math.ceil(totalDataCount / limit)
-        upToPageTotalData = Math.min(pageNumber * limitNumber, totalDataCount)
-        startPageData = skip + 1;
-    }
-    const pageDataCount = await model.countDocuments(queryResult)
-    const data = await queryResult
-
-    return {
-        startPageData,
-        pageDataCount,
-        numberOfPages,
-        totalDataCount,
-        data,
-        upToPageTotalData,
-    }
-
-}
+  return {
+    startPageData,
+    pageDataCount,
+    numberOfPages,
+    totalDataCount,
+    data,
+    upToPageTotalData,
+  };
+};
